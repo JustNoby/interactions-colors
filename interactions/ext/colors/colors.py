fe9ec37251f67a25caf51e539e936ffafde9fdb8
JustNoby15 <justnobodysilver@gmail.com>
2022-06-10 22:33:08 -0400

    first commit

---
+++ interactions/ext/colors/colors.py
@@ -0,0 +1,18 @@
+from interactions import Client, Extension
+from typing import Optional
+from typing_extensions import Self
+
+
+class interactionsColor(Extension):
+    """
+    A Class Color
+    """
+    @classmethod
+    def green(cls) -> Self:
+        """Returns green"""
+        return int("3066993")
+
+def setup() -> Optional[Extension]:
+    return interactionsColor
+
+print(interactionsColor.green())
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="interactions.ext.colors",
+    version="0.7",
+    description="Add a color function to Embed",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JustNoby/interactions-colors",
+    author="JustNoby",
+    author_email="justnobodysilver@gmail.com",
+    license="MIT",
+    packages=["interactions.ext.colors"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "discord-py-interactions>=4.1.0"
+    ],
+)
