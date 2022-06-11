e891c1d04e6160c49400d18a6af372a6c889e0f2
JustNoby15 <justnobodysilver@gmail.com>
2022-06-10 22:38:49 -0400

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions.ext.colors",
-    version="0.7",
+    version="0.1",
     description="Add a color function to Embed",
     long_description=long_description,
     long_description_content_type="text/markdown",
